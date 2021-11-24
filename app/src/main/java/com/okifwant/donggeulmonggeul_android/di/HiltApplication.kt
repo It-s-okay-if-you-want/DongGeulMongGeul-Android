e3f82ff6a7313f24a3416cf1ffcce8071acc5da7
minjun8946 <minjun8946@daum.net>
2021-11-24 16:45:26 +0900

    base

---
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/di/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.okifwant.donggeulmonggeul_android.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application(){
+}
---
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.okifwant.donggeulmonggeul_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.okifwant.donggeulmonggeul_android.R
+import com.okifwant.donggeulmonggeul_android.base.BaseActivity
+import com.okifwant.donggeulmonggeul_android.databinding.ActivityMainBinding
+import com.okifwant.donggeulmonggeul_android.viewmodel.MainViewModel
+
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(){
+
+    override val layoutId = R.layout.activity_main
+    override val viewModel : MainViewModel by viewModels()
+
+    override fun init() {
+
+    }
+
+}
