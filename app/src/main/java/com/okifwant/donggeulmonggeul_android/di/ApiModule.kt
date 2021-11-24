56ef7c159c5a0e85cb77b650b3faf8a37016b772
minjun8946 <minjun8946@daum.net>
2021-11-24 17:39:13 +0900

    api module

---
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/di/ApiModule.kt
@@ -0,0 +1,41 @@
+package com.okifwant.donggeulmonggeul_android.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    private const val BASE_URL = "https://api.noonbody.me"
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addNetworkInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(providesOkHttpClient(providesHttpLoggingInterceptor()))
+        .build()
+}
