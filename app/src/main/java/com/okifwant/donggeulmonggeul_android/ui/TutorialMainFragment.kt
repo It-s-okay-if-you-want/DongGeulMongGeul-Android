34dece4a9c10ae3ec7682704c920a44d526a8099
ParkSangSun <appleasz1028102@gmail.com>
2021-11-25 08:54:08 +0900

    [CREATE] TutorialMainFragment.kt

---
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/TutorialMainFragment.kt
@@ -0,0 +1,17 @@
+package com.okifwant.donggeulmonggeul_android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.okifwant.donggeulmonggeul_android.R
+import com.okifwant.donggeulmonggeul_android.base.BaseFragment
+import com.okifwant.donggeulmonggeul_android.databinding.FragmentTutorialMainBinding
+
+
+class TutorialMainFragment : BaseFragment<FragmentTutorialMainBinding>(R.layout.fragment_tutorial_main) {
+    override fun init() {
+
+    }
+}
