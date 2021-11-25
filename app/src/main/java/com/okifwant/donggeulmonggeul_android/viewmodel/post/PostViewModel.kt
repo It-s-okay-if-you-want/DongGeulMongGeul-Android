24fc8f631fea7cf82c00f00142a8c976e74506a2
Jaewon <turitus@naver.com>
2021-11-25 14:00:28 +0900

    [UPDATE] set category

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/viewmodel/post/PostViewModel.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/viewmodel/post/PostViewModel.kt
@@ -1,6 +1,9 @@
 package com.okifwant.donggeulmonggeul_android.viewmodel.post
 
+import androidx.lifecycle.MutableLiveData
 import com.okifwant.donggeulmonggeul_android.base.BaseViewModel
 
 class PostViewModel: BaseViewModel() {
+
+    val category = MutableLiveData("카테고리")
 }
