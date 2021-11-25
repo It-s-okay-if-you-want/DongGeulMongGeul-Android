16b70577721470c650922939bb00d9b9148f1afa
Jaewon <turitus@naver.com>
2021-11-25 15:47:26 +0900

    [UPDATE] dialog style

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/dialog/PostCategoryDialog.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/dialog/PostCategoryDialog.kt
@@ -1,23 +1,29 @@
 package com.okifwant.donggeulmonggeul_android.ui.dialog
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.okifwant.donggeulmonggeul_android.R
 import com.okifwant.donggeulmonggeul_android.databinding.DialogCategoryPostBinding
 import com.okifwant.donggeulmonggeul_android.viewmodel.post.PostViewModel
 
 class PostCategoryDialog(private val vm: PostViewModel): BottomSheetDialogFragment() {
     private lateinit var binding: DialogCategoryPostBinding
 
+    override fun getTheme(): Int = R.style.CustomBottomSheetDialogTheme
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = DialogCategoryPostBinding.inflate(LayoutInflater.from(context))
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.dialog_category_post, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
