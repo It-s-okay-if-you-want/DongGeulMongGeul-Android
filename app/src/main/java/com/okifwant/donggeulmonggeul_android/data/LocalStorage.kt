e44c54489d57a015df016c521ea20839466199ea
Jaewon <turitus@naver.com>
2021-11-25 20:54:36 +0900

    [UPDATE] todo token

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/data/LocalStorage.kt
+++
@@ -1,5 +0,0 @@
-package com.okifwant.donggeulmonggeul_android.data
-
-interface LocalStorage {
-    fun getToken(): String
-}
--- app/src/main/java/com/okifwant/donggeulmonggeul_android/data/post/PostDataSource.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/data/post/PostDataSource.kt
@@ -1,20 +1,20 @@
 package com.okifwant.donggeulmonggeul_android.data.post
 
-import com.okifwant.donggeulmonggeul_android.data.LocalStorage
 import com.okifwant.donggeulmonggeul_android.data.post.dto.PostImageResponse
 import com.okifwant.donggeulmonggeul_android.data.post.dto.PostRequest
 import com.okifwant.donggeulmonggeul_android.data.post.dto.PostTogetherRequest
 import okhttp3.MultipartBody
 import retrofit2.Response
 
-class PostDataSource(private val postApi: PostApi, private val localStorage: LocalStorage) {
+class PostDataSource(private val postApi: PostApi) {
 
     suspend fun postImage(image: MultipartBody.Part): Response<PostImageResponse> =
            postApi.postImage(image)
 
+    //TODO 토큰 얻어와서 넣기
     suspend fun post(postRequest: PostRequest): Response<Unit> =
-        postApi.post(localStorage.getToken(), postRequest)
+        postApi.post("todo-토큰넣어야해", postRequest)
 
     suspend fun postTogether(request: PostTogetherRequest) =
-        postApi.postGroup(localStorage.getToken(), request)
+        postApi.postGroup("todo-토큰넣어야", request)
 }
