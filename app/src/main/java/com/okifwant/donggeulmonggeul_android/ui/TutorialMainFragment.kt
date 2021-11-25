d4fc569ebf71e486d9385df990a65efe04dc693f
ParkSangSun <appleasz1028102@gmail.com>
2021-11-25 20:26:51 +0900

    init

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/TutorialMainFragment.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/TutorialMainFragment.kt
@@ -2,18 +2,20 @@ package com.okifwant.donggeulmonggeul_android.ui
 
 import android.util.Log
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.okifwant.donggeulmonggeul_android.R
 import com.okifwant.donggeulmonggeul_android.adapter.tutorial.PagerRecyclerAdapter
 import com.okifwant.donggeulmonggeul_android.base.BaseFragment
+import com.okifwant.donggeulmonggeul_android.base.SangSunBaseFragment
 import com.okifwant.donggeulmonggeul_android.databinding.FragmentTutorialMainBinding
+import com.okifwant.donggeulmonggeul_android.viewmodel.CategoryViewModel
 import com.okifwant.donggeulmonggeul_android.viewmodel.TutorialViewModel
 
 
 class TutorialMainFragment :
-    BaseFragment<FragmentTutorialMainBinding>(R.layout.fragment_tutorial_main) {
+    SangSunBaseFragment<FragmentTutorialMainBinding>(R.layout.fragment_tutorial_main) {
     private val viewModel by activityViewModels<TutorialViewModel>()
-
 
     override fun init() {
         initViewPager()
@@ -28,11 +30,11 @@ class TutorialMainFragment :
 
     private fun observeViewModel() {
         viewModel.startBtnEvent.observe(this, { page ->
-            Log.d("TAG","page info : $page")
-           /* when (page) {
-                3 -> binding.startBtn.visibility = View.VISIBLE
-                else -> binding.startBtn.visibility = View.INVISIBLE
-            }*/
+            Log.d("TAG", "page info : $page")
+            /* when (page) {
+                 3 -> binding.startBtn.visibility = View.VISIBLE
+                 else -> binding.startBtn.visibility = View.INVISIBLE
+             }*/
         })
     }
 }
