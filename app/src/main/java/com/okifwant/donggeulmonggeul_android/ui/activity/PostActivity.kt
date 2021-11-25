0f76374c1f14f3a01f414a8153183d673ae5b002
Jaewon <turitus@naver.com>
2021-11-25 13:40:22 +0900

    [MODIFY] post fragment to activity

---
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/activity/PostActivity.kt
@@ -0,0 +1,14 @@
+package com.okifwant.donggeulmonggeul_android.ui.activity
+
+import androidx.activity.viewModels
+import com.okifwant.donggeulmonggeul_android.R
+import com.okifwant.donggeulmonggeul_android.base.BaseActivity
+import com.okifwant.donggeulmonggeul_android.databinding.ActivityPostBinding
+import com.okifwant.donggeulmonggeul_android.viewmodel.post.PostViewModel
+
+class PostActivity : BaseActivity<ActivityPostBinding, PostViewModel>() {
+
+    override val layoutId: Int = R.layout.activity_post
+
+    override val viewModel: PostViewModel by viewModels()
+}
