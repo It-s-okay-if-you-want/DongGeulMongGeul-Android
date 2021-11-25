bfb569feb6693981dfe055d6b6ae7bdc428ab03c
Jaewon <turitus@naver.com>
2021-11-25 23:22:52 +0900

    [UPDATE] post detail ui

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/activity/PostDetailActivity.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/activity/PostDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.okifwant.donggeulmonggeul_android.ui.activity
 
-import android.os.Bundle
 import androidx.activity.viewModels
 import com.okifwant.donggeulmonggeul_android.R
 import com.okifwant.donggeulmonggeul_android.base.BaseActivity
@@ -12,10 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class PostDetailActivity : BaseActivity<ActivityPostDetailBinding, PostDetailViewModel>() {
     override val layoutId: Int = R.layout.activity_post_detail
     override val viewModel: PostDetailViewModel by viewModels()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
-    }
 
 
 }
