5fd4a50d4569261ccfd17ecc831c6e0f71c979f6
ParkSangSun <appleasz1028102@gmail.com>
2021-11-25 15:31:34 +0900

    [CREATE] CategoryFragment.kt

--- app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/fragment/CategoryFragment.kt
+++ app/src/main/java/com/okifwant/donggeulmonggeul_android/ui/fragment/CategoryFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.okifwant.donggeulmonggeul_android.R
+import com.okifwant.donggeulmonggeul_android.base.BaseFragment
+import com.okifwant.donggeulmonggeul_android.databinding.FragmentCategoryBinding
+import com.okifwant.donggeulmonggeul_android.viewmodel.CategoryViewModel
+import com.okifwant.donggeulmonggeul_android.viewmodel.MainViewModel
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : BaseFragment<FragmentCategoryBinding, CategoryViewModel>() {
+    override val layoutId = R.layout.fragment_category
+    override val viewModel : CategoryViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+
+    override fun init() {
+
     }
 }
